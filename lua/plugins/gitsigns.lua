2a3b8990e674eb17933f47e43f0499aebfb305d6
agaldamez <agaldamez@protonmail.ch>
2026-09-13 08:32:04 -0700

    testing

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -37,16 +37,8 @@ return {
         end)
 
         -- Actions
-        map('n', '<leader>hs', gitsigns.stage_hunk)
-        map('n', '<leader>hr', gitsigns.reset_hunk)
-
-        map('v', '<leader>hs', function()
-          gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-        end)
-
-        map('v', '<leader>hr', function()
-          gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-        end)
+        map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+        map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
 
         map('n', '<leader>hS', gitsigns.stage_buffer)
         map('n', '<leader>hR', gitsigns.reset_buffer)
